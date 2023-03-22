81d93117d6adf5b7bb3dbe81f620ac35088f5ecc
Ricky Stoll <rwstoll@iu.edu>
2023-03-22 17:51:04 -0400

    added find all

---
+++ src/main/java/edu/iu/c322/customerservice/model/Customer.java
@@ -0,0 +1,46 @@
+package edu.iu.c322.customerservice.model;
+
+import java.util.Objects;
+
+public class Customer {
+    private int id;
+    private String name;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return id == customer.id && Objects.equals(name, customer.name) && Objects.equals(email, customer.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email);
+    }
+}
